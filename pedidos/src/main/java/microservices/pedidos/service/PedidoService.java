00519ac44aff33792a3e6ce9ba8456c7faa2a7ae
Ademir <ademirjuniorfertonani@gmail.com>
2026-03-05 18:24:19 -0300

    Impl service

---
+++ pedidos/src/main/java/microservices/pedidos/service/PedidoService.java
@@ -0,0 +1,34 @@
+package microservices.pedidos.service;
+
+import microservices.pedidos.model.ItemPedido;
+import microservices.pedidos.model.Pedido;
+import microservices.pedidos.repository.PedidosRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PedidoService {
+
+    private final PedidosRepository pedidosRepository;
+
+    public PedidoService(PedidosRepository pedidosRepository){
+        this.pedidosRepository = pedidosRepository;
+    }
+
+    public Pedido salvarPedido(Pedido pedido){
+        if(pedido.getItens() != null){
+            for(ItemPedido item : pedido.getItens()) {
+                item.setPedido(pedido);
+            }
+        }
+        return pedidosRepository.save(pedido);
+    }
+
+    public List<Pedido> listarPedidos(){
+        return pedidosRepository.findAll();
+    }
+
+
+    
+}
