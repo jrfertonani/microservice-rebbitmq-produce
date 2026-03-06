8354366f0ef71743f3a882799edab8c7ce802ab9
Ademir <ademirjuniorfertonani@gmail.com>
2026-03-05 21:40:39 -0300

    Impl produc

--- pedidos/src/main/java/microservices/pedidos/controller/PedidoController.java
+++ pedidos/src/main/java/microservices/pedidos/controller/PedidoController.java
@@ -2,6 +2,8 @@ package microservices.pedidos.controller;
 
 import microservices.pedidos.model.Pedido;
 import microservices.pedidos.service.PedidoService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,17 +12,21 @@ import java.util.List;
 @RequestMapping("/pedidos")
 public class PedidoController {
 
+    public final RabbitTemplate rabbitTemplate;
     public final PedidoService pedidoService;
 
-    public PedidoController (PedidoService pedidoService){
+    public PedidoController (PedidoService pedidoService, RabbitTemplate rabbitTemplate){
         this.pedidoService = pedidoService;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
+    @Value("${broker.queue.processamento.name}")
+    private String routingKey;
 
     @PostMapping
     public String criarPedido(@RequestBody Pedido pedido){
-        pedidoService.salvarPedido(pedido);
-
+       Pedido pedidoSalvo = pedidoService.salvarPedido(pedido);
+        rabbitTemplate.convertAndSend("",routingKey,pedidoSalvo.getDescricao());
         return "Pedido salvo e enviado para processamento: " +pedido.getDescricao();
     }
 
